8b590707d199a4a50cc3b9eb3772b1e64fa4fffa
Christabel <udodorchristabel@gmail.com>
2024-03-28 19:41:11 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded',() =>{
+//     const buyButtons = document.querySelectorAll('.buy');
+//     buyButtons.forEach(button => {
+//       button.addEventListener('click', function() {
+//         const cardBody = this.parentElement;
+        
+//         const itemName = this.dataset.item;
+//         const itemDescription = this.dataset.description;
+        // console.log(itemDescription);
+        // const itemPrice = parseFloat(this.dataset.price);
+        // const img = this.dataset.image
+        
+        // Redirect to order.html with item information in the URL
+  //       window.location.href = `order.html?itemName=${itemName}&itemDescription=${itemDescription}&itemPrice=${itemPrice}&img=${img}`;
+  //     });
+  //   });
+  // });
+
